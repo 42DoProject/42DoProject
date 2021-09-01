ba2902f35da0ae06f9a87c9dfbf42845af1f0d4a
Seoyoung Jeong <issyo1022@gmail.com>
2021-09-01 23:09:17 +0900

    feat: init profilePage, add route, fix warnings

--- front/src/Components/MainPage/Dashboard.js
+++ front/src/Components/MainPage/Dashboard.js
@@ -29,9 +29,9 @@ export default function Dashboard() {
               <div className="column2__job">프론트엔드</div>
               <div className="column2__level">초보</div>
               <div className="column2__skill">
-                <img src="https://img.shields.io/badge/HTML-E34F26?style=flat-square&logo=HTML5&logoColor=white" />
-                <img src="https://img.shields.io/badge/CSS-1572B6?style=flat-square&logo=CSS3&logoColor=white" />
-                <img src="https://img.shields.io/badge/Javascript-F7DF1E?style=flat-square&logo=JavaScript&logoColor=white" />
+                <img alt="badge1" src="https://img.shields.io/badge/HTML-E34F26?style=flat-square&logo=HTML5&logoColor=white" />
+                <img alt="badge2" src="https://img.shields.io/badge/CSS-1572B6?style=flat-square&logo=CSS3&logoColor=white" />
+                <img alt="badge3" src="https://img.shields.io/badge/Javascript-F7DF1E?style=flat-square&logo=JavaScript&logoColor=white" />
               </div>
             </div>
           </div>
---
+++ front/src/Components/ProfilePage/ProfileHeader.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../SCSS/ProfilePage/ProfileHeader.scss";
+
+export default function ProfileHeader(){
+	return (
+	<div className="profileHeader">
+		<span className="hello">HEllo</span>
+	</div>
+	);
+}

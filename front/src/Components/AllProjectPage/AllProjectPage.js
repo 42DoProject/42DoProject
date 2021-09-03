c8ef5132f64250d19272d3021d6fe1c8ee3135d7
jiyong LEE <jyman1995@gmail.com>
2021-09-03 18:33:15 +0900

    feat: #24 Cards margin-right modify + Router

--- front/src/Components/AllProjectPage/AllProjectPage.js
+++ front/src/Components/AllProjectPage/AllProjectPage.js
@@ -2,13 +2,17 @@ import React from "react";
 import Navbar from "../CommonComponent/Navbar";
 import ProjectContents from "./ProjectContents";
 import ProjectTypeBar from "./ProjectTypeBar";
-import "../../SCSS/AllProjectPage/ProjectTypeBar.scss"
+import "../../SCSS/AllProjectPage/ProjectTypeBar.scss";
+import "../../SCSS/AllProjectPage/AllProjectPage.scss";
+
 export default function AllProjectPage() {
-	return (
-	  <>
-		<Navbar />
-		<ProjectTypeBar />
-		<ProjectContents />
-	  </>
-	);
-  }
+  return (
+    <>
+      <Navbar />
+      <div className="allproject__wrap">
+        <ProjectTypeBar />
+        <ProjectContents />
+      </div>
+    </>
+  );
+}

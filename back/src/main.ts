10c337cbaf1e6e08828f90ec145e8fc29e0875c9
djeon <du0928@naver.com>
2021-09-04 22:06:54 +0900

    feat: #12 #23 add MySQL container and install Sequelize

--- back/src/main.ts
+++ back/src/main.ts
@@ -2,6 +2,13 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import auth from "./auth/auth.controller";
 
+const { sequelize } = require('../models');
+
+sequelize.sync({ force: false })
+.then(() => {
+    console.log('데이터베이스 연결 성공');
+});
+
 dotenv.config();
 
 async function bootstrap() {

5b12a34f16e32a5058ddd74e791007ea0ab5ad0c
jiyong LEE <jyman1995@gmail.com>
2021-09-11 14:20:19 +0900

    feat: #60 MainPage Dashboard Data Binding

---
+++ front/src/Components/MainPage/DashCards.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Icon } from "@iconify/react";
+import "../../SCSS/MainPage/DashCards.scss";
+export default function DashCards() {
+  let store = useSelector((store) => {
+    return store;
+  });
+  let userState = store.userReducer;
+  return (
+    <div className="DashCards">
+      <div className="DashCards__image">
+        <img src={userState.progressProject[0].image} alt={"progress" + 1} />
+        <div className="DashCards__title">
+          {userState.progressProject[0].title}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ front/src/Components/MainPage/FavoriteSlide.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import DashCards from "./DashCards";
+import { Icon } from "@iconify/react";
+import "../../SCSS/MainPage/DashSlide.scss";
+export default function FavoriteSlide() {
+  let [slideFlag, setSlideFlag] = useState([0, 0]);
+  return (
+    <div className="dashSlide">
+      <button
+        className="cardbutton prev"
+        onClick={(event) => {
+          prevUtil(slideFlag, setSlideFlag);
+        }}
+      >
+        <Icon className="prev__icon" icon="dashicons:arrow-left-alt2" />
+      </button>
+      <div className="dashCardslist">
+        <div className="dashCards-wrap">
+          <DashCards />
+          <DashCards />
+          <DashCards />
+          <DashCards />
+          <DashCards />
+          <DashCards />
+          <DashCards />
+          <DashCards />
+          <DashCards />
+          <DashCards />
+          <DashCards />
+          <DashCards />
+          <DashCards />
+          <DashCards />
+          <DashCards />
+          <DashCards />
+        </div>
+      </div>
+      <button
+        className="cardbutton next"
+        onClick={(event) => {
+          nextUtil(slideFlag, setSlideFlag);
+        }}
+      >
+        <Icon className="next__icon" icon="dashicons:arrow-right-alt2" />
+      </button>
+    </div>
+  );
+}
+
+function nextUtil(slideFlag, setSlideFlag) {
+  const $dashCards = document.querySelector(
+    ".dashboard .row2__box3 .dashCards-wrap"
+  );
+  console.log($dashCards);
+  $dashCards.style.transition = "transform .7s ease-out";
+  slideFlag[1]++;
+  $dashCards.style.transform = `translateX(${
+    (223 / 16) * slideFlag[0] + (-223 / 16) * slideFlag[1]
+  }rem)`;
+  let newSlideFlag = [slideFlag[0], slideFlag[1]];
+  setSlideFlag(newSlideFlag);
+}
+
+function prevUtil(slideFlag, setSlideFlag) {
+  const $dashCards = document.querySelector(
+    ".dashboard .row2__box3 .dashCards-wrap"
+  );
+  $dashCards.style.transition = "transform .7s ease-out";
+  if (slideFlag[1] !== 0) slideFlag[1]--;
+  $dashCards.style.transform = `translateX(${
+    (223 / 16) * slideFlag[0] + (-223 / 16) * slideFlag[1]
+  }rem)`;
+  let newSlideFlag = [slideFlag[0], slideFlag[1]];
+  setSlideFlag(newSlideFlag);
+}

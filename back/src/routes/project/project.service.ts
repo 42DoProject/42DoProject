cd1cb73a32181f934ce772eb5362a021218dc812
djeon <du0928@naver.com>
2021-09-15 00:23:16 +0900

    feat: #73 #78 add project list to table and delete project list from table

--- back/src/routes/project/project.service.ts
+++ back/src/routes/project/project.service.ts
@@ -14,24 +14,36 @@ const pagination = async (request: Request, response: Response, state: string) =
     if (page !== undefined && pageSize !== undefined) {
         limit = Number(pageSize);
         offset = (Number(page) - 1) * limit;
-        project = await Project.findAll({
-            include: {
-                model: Projecttag,
-                include: [{ model: Tag, where: { tagTitle: tag }, required: true }],
-                required: true
-            },
-            where: { state: state },
-            offset: offset,
-            limit: limit
-        });
+        if (tag !== undefined) {
+            project = await Project.findAll({
+                include: {
+                    model: Projecttag,
+                    include: [{ model: Tag, where: { tagTitle: tag }, required: true }],
+                    required: true
+                },
+                where: { state: state },
+                offset: offset,
+                limit: limit
+            });
+        } else {
+            project = await Project.findAll({
+                where: { state: state },
+                offset: offset,
+                limit: limit
+            });
+        }
     } else if (page === undefined && pageSize === undefined) {
-        project = await Project.findAll({ 
-            include: {
-                model: Projecttag,
-                include: [{ model: Tag, where: { tagTitle: tag }, required: true }],
-                required: true
-            },
-            where: { state: state } });
+        if (tag !== undefined) {
+            project = await Project.findAll({ 
+                include: {
+                    model: Projecttag,
+                    include: [{ model: Tag, where: { tagTitle: tag }, required: true }],
+                    required: true
+                },
+                where: { state: state } });
+        } else {
+            project = await Project.findAll({ where: { state: state } });
+        }
     } else {
         response.status(400).json({ error: "invalid query" });
         return;
@@ -64,4 +76,56 @@ export const getList = async (request: Request, response: Response) => {
         return;
     }
     response.status(200).json({ project });
+}
+
+export const postList = async (request: Request, response: Response) => {
+    const { title, totalMember, currentMember, state, tag } = request.body;
+    const date = new Date();
+    let tagTable;
+    await Project.create({
+    	title: title,
+        totalMember: totalMember,
+        currentMember: currentMember,
+        state: state,
+        like: 0,
+        createdAt: date,
+        updatedAt: date
+    })
+    .then(async project => {
+        if (tag !== undefined) {
+            tagTable = await Tag.findAll({ attributes: ['id'], where: { tagTitle: tag } });
+            tagTable.forEach( async (element) => {
+                await Projecttag.create({
+                    projectId: project.id,
+                    tagId: element.id
+                })
+            })
+            response.status(200).json({ message: 'added successfully with tags.', project });
+        }
+        else {
+            response.status(200).json({ message: 'added successfully.', project });
+        }
+    })
+    .catch(err => {
+    	response.status(400).json({ message: String(err) });
+    })
+}
+
+export const deleteList = async (request: Request, response: Response) => {
+    const { id } = request.query;
+    await Projecttag.destroy({
+        where: { projectId: id }
+    })
+    .catch(err => {
+        response.status(400).json({ message: String(err) });
+    });
+    await Project.destroy({
+        where: { id: id }
+    })
+    .then(result => {
+        response.status(200).json({ message: 'deleted successfully.' });
+    })
+    .catch(err => {
+        response.status(400).json({ message: String(err) });
+    });
 }

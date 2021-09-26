79cf5a6ebd9c59f7b0a51539a57a80b1ac231329
Seoyoung Jeong <issyo1022@gmail.com>
2021-09-27 02:09:12 +0900

    feat: #6 change whole profile page design

--- front/src/Components/ProfilePage/ProfileBody.js
+++ front/src/Components/ProfilePage/ProfileBody.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Icon } from "@iconify/react";
 import "../../SCSS/ProfilePage/ProfileBody.scss";
-
+// import Cards from "../MainPage/Cards";
 export default function ProfileBody() {
   return (
     <div className="profileBody">
-      <div className="profileBody__row1">
-        <div className="row1__profile-card1">
+      <div className="profileBody__col1">
+        <div className="col1__profile-card1">
           <div className="card1__column1">
             <div className="column1__job">직업</div>
             <div className="column1__level">레벨</div>
@@ -28,10 +28,22 @@ export default function ProfileBody() {
                 alt="badge3"
                 src="https://img.shields.io/badge/Javascript-F7DF1E?style=flat-square&logo=JavaScript&logoColor=white"
               />
+              <img
+                alt="badge1"
+                src="https://img.shields.io/badge/HTML-E34F26?style=flat-square&logo=HTML5&logoColor=white"
+              />
+              <img
+                alt="badge2"
+                src="https://img.shields.io/badge/CSS-1572B6?style=flat-square&logo=CSS3&logoColor=white"
+              />
+              <img
+                alt="badge3"
+                src="https://img.shields.io/badge/Javascript-F7DF1E?style=flat-square&logo=JavaScript&logoColor=white"
+              />
             </div>
           </div>
         </div>
-        <div className="row1__profile-card2">
+        <div className="col1__profile-card2">
           <div className="card2__location">
             <Icon icon="carbon:location-filled" height="25px" />
             <span>Seoul</span>
@@ -46,20 +58,25 @@ export default function ProfileBody() {
           </div>
         </div>
       </div>
-      <div className="profileBody__row2">
-        <div className="row2__introduction">
-          풀스택 개발자 지망생 jiylee입니다! 포트폴리오 겸 프론트엔드 쪽으로
-          프로젝트 구하고 있습니다. 채팅 주세요~
-        </div>
-        <hr />
-        <div className="row2__ongoing-projects">
-          <span className="row2__subject-span">진행중인 프로젝트</span>
+      <div className="profileBody__col2">
+        <div className="col2__ongoing-projects">
+          <span className="col2__subject-span">진행중인 프로젝트</span>
           <span>없음</span>
         </div>
-        <hr />
-        <div className="row2__done-projects">
-          <span className="row2__subject-span">완료한 프로젝트</span>
+        <hr className="hr__line" />
+        <div className="col2__done-projects">
+          <span className="col2__subject-span">완료한 프로젝트</span>
           <span>없음</span>
+        </div>
+        <hr className="hr__line" />
+        <div className="col2__interested-projects">
+          <span className="col2__subject-span">관심있는 프로젝트</span>
+          <span>없음</span>
+          <div className="interested__cards">
+            {/* <Cards />
+            <Cards />
+            <Cards /> */}
+          </div>
         </div>
       </div>
     </div>
--- front/src/Components/ProfilePage/ProfileHeader.js
+++ front/src/Components/ProfilePage/ProfileHeader.js
@@ -1,29 +1,36 @@
 import React from "react";
 import "../../SCSS/ProfilePage/ProfileHeader.scss";
 import { Icon } from "@iconify/react";
-import ProfileBody from "./ProfileBody";
-import ProfileRight from "./ProfileRight";
 
 export default function ProfileHeader() {
   return (
     <div className="profileHeader">
-      <div className="profileImage">
-        <img alt="profileImage1" />
-        <div className="profileImage__icons">
-          <Icon className="profile__icon1" icon="ion:chatbox-ellipses" />
-          <Icon
-            className="profile__icon2"
-            icon="ant-design:user-add-outlined"
-          />
-        </div>
-        <div className="profile__name">
-          jiylee
-          <span className="profile__level">lv.4</span>
-        </div>
+      <div className="header__left">
+        <img className="profileImage" alt="profileImage1" />
+
         <div className="profile__bubble">
           리액트를 이용한 웹 프론트엔드 개발을 해보고 싶습니다!
         </div>
         <div className="profile__last-access">마지막 접속: 3일 전</div>
+      </div>
+      <div className="header__right">
+        <div className="right__row1">
+          <div className="row1__name">jiylee</div>
+          <button className="row1__send-message">메시지 보내기</button>
+          <button className="row1__follow">팔로우</button>
+        </div>
+        <div className="right__row2">
+          <div className="row2__status">프로젝트 찾는 중</div>
+          <div className="row2__follower">팔로워 10명</div>
+          <div className="row2__following">팔로워 15명</div>
+        </div>
+        <div className="right__introduction">
+          풀스택 개발자 지망생 jiylee입니다! <br />
+          <br />
+          포트폴리오 겸 프론트엔드 쪽으로 프로젝트 구하고 있습니다. <br />
+          <br />
+          채팅 주세요~
+        </div>
       </div>
     </div>
   );

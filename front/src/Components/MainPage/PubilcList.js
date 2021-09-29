1026219fe459b73f16eb4b4b3fcef4cd2748dd11
jiyong LEE <jyman1995@gmail.com>
2021-09-29 17:47:12 +0900

    feat: #139 Fix DataBinding

--- front/src/Components/MainPage/PubilcList.js
+++ front/src/Components/MainPage/PubilcList.js
@@ -2,60 +2,78 @@ import Cards from "./Cards";
 import "../../SCSS/MainPage/List.scss";
 import { Icon } from "@iconify/react";
 import { useEffect, useState } from "react";
+import axios from "axios";
+import ReactLoading from "./MainLoading";
 
-export default function Cardlist(props) {
-  let [slideFlag, setSlideFlag] = useState([0, 0]);
-  console.log("publiclist");
+export default function Cardlist() {
+  let [slideFlag, setSlideFlag] = useState(0);
+  const [completedProject, setCompletedProject] = useState([]);
+  const getData = async () => {
+    try {
+      const {
+        data: { project: completedData },
+      } = await axios.get(
+        `http://localhost:5000/project?state=completed&pageSize=20&page=1`
+      );
+      setCompletedProject(completedData);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   useEffect(() => {
-    console.log("publiclist-effect", props.completedProject);
-  }, [props.completedProject]);
+    getData();
+  }, []);
   return (
-    <div className="cardlist">
-      <button
-        className="cardbutton prev"
-        onClick={(event) => {
-          prevUtil(slideFlag, setSlideFlag);
-        }}
-      >
-        <Icon className="prev__icon" icon="dashicons:arrow-left-alt2" />
-      </button>
-      <button
-        className="cardbutton next"
-        onClick={(event) => {
-          nextUtil(slideFlag, setSlideFlag);
-        }}
-      >
-        <Icon className="next__icon" icon="dashicons:arrow-right-alt2" />
-      </button>
-      <div className="cardpadding">
-        <div className="cards-row">
-          {props.completedProject.map((el, idx) => {
-            return <Cards key={idx} projectData={el} />;
-          })}
+    <>
+      {completedProject.length === 0 ? (
+        <ReactLoading type="spin" color="#a7bc5b" />
+      ) : (
+        <div className="cardlist">
+          {slideFlag === 0 ? null : (
+            <button
+              className="cardbutton prev"
+              onClick={(event) => {
+                prevUtil(slideFlag, setSlideFlag);
+              }}
+            >
+              <Icon className="prev__icon" icon="dashicons:arrow-left-alt2" />
+            </button>
+          )}
+          {slideFlag >= (completedProject.length - 4) / 2 ? null : (
+            <button
+              className="cardbutton next"
+              onClick={(event) => {
+                nextUtil(slideFlag, setSlideFlag, completedProject.length);
+              }}
+            >
+              <Icon className="next__icon" icon="dashicons:arrow-right-alt2" />
+            </button>
+          )}
+          <div className="cardpadding">
+            <div className="cards-row">
+              {completedProject.map((el, idx) => {
+                return <Cards key={idx} projectData={el} />;
+              })}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
-function nextUtil(slideFlag, setSlideFlag) {
+function nextUtil(slideFlag, setSlideFlag, prCnt) {
   const $cardsrow = document.querySelector(".public .cards-row");
   $cardsrow.style.transition = "transform .7s ease-out";
-  if (slideFlag[1] !== 3) slideFlag[1]++;
-  $cardsrow.style.transform = `translateX(${
-    (696 / 16) * slideFlag[0] + (-696 / 16) * slideFlag[1]
-  }rem)`;
-  let newSlideFlag = [slideFlag[0], slideFlag[1]];
-  setSlideFlag(newSlideFlag);
+  if (slideFlag[1] <= (prCnt - 4) / 2) slideFlag++;
+  $cardsrow.style.transform = `translateX(${+(-696 / 16) * slideFlag}rem)`;
+  setSlideFlag(slideFlag);
 }
 
 function prevUtil(slideFlag, setSlideFlag) {
-  const $cardsrow = document.querySelector(".public .cards-row");
+  const $cardsrow = document.querySelector(".recruit .cards-row");
   $cardsrow.style.transition = "transform .7s ease-out";
-  if (slideFlag[1] !== 0) slideFlag[1]--;
-  $cardsrow.style.transform = `translateX(${
-    (696 / 16) * slideFlag[0] + (-696 / 16) * slideFlag[1]
-  }rem)`;
-  let newSlideFlag = [slideFlag[0], slideFlag[1]];
-  setSlideFlag(newSlideFlag);
+  if (slideFlag !== 0) slideFlag--;
+  $cardsrow.style.transform = `translateX(${(-696 / 16) * slideFlag}rem)`;
+  setSlideFlag(slideFlag);
 }

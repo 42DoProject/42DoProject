e76217186ebde64c451df85b380ba080ef877a7f
Seoyoung Jeong <issyo1022@gmail.com>
2021-10-01 03:43:13 +0900

    feat: #134 delete level, add letter-counter etc

--- front/src/Components/ProfileEditPage/ProfileEditHeader.js
+++ front/src/Components/ProfileEditPage/ProfileEditHeader.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import "../../SCSS/ProfilePage/ProfileHeader.scss";
 // import { Icon } from "@iconify/react";
@@ -6,6 +6,8 @@ import "../../SCSS/ProfilePage/ProfileHeader.scss";
 export default function ProfileEditHeader() {
   // let userState = useSelector((state) => state.userReducer);
   let loginState = useSelector((state) => state.loginReducer);
+  let [bubbleLength, setBubbleLength] = useState(0);
+  let [introLength, setIntroLength] = useState(0);
 
   return (
     <div className="profileHeader">
@@ -15,12 +17,20 @@ export default function ProfileEditHeader() {
           alt="profileImage"
           src={loginState.profileImage}
         />
-
-        <textarea
-          spellcheck="false"
-          className="profile__bubble"
-          maxlength="30"
-        />
+        <div className="bubble__wrapper">
+          <textarea
+            spellCheck="false"
+            className="profile__bubble"
+            maxLength="30"
+            placeholder="ex. 리액트를 이용한 웹 프론트엔드 개발을 해보고 싶습니다!"
+            onChange={(e) => {
+              if (e.target.value.length > e.target.maxLength) {
+                e.target.value.substr(0, e.target.maxLength); //한글 처리
+              } else setBubbleLength(e.target.value.length);
+            }}
+          />
+          <div className="bubble__letters-count">{bubbleLength} / 30</div>
+        </div>
         <div className="profile__last-access">마지막 접속: 3일 전</div>
       </div>
       <div className="header__right">
@@ -40,7 +50,20 @@ export default function ProfileEditHeader() {
           <div className="row2__follower">팔로워 10명</div>
           <div className="row2__following">팔로워 15명</div>
         </div>
-        <textarea spellcheck="false" className="right__introduction" />
+        <div className="right__introduction">
+          <textarea
+            spellCheck="false"
+            className="introduction__textarea"
+            maxLength="500"
+            placeholder="자기소개를 작성해주세요. (ex. 내가 잘하는 분야, 하고 싶은 프로젝트, 배우고 싶은 스킬 등)"
+            onChange={(e) => {
+              if (e.target.value.length > e.target.maxLength) {
+                e.target.value.substr(0, e.target.maxLength); //한글 처리
+              } else setIntroLength(e.target.value.length);
+            }}
+          />
+          <div className="introduction__letters-count">{introLength} / 500</div>
+        </div>
       </div>
     </div>
   );

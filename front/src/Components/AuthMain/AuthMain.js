452c3234809f2774e6f49aa61fd93e4cddfd1b51
jiyong LEE <jyman1995@gmail.com>
2021-10-03 00:41:04 +0900

    fix: #179 Fix 42API 401 ERR

--- front/src/Components/AuthMain/AuthMain.js
+++ front/src/Components/AuthMain/AuthMain.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useHistory, useLocation } from "react-router";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
@@ -10,28 +10,7 @@ export default function AuthMain() {
   let dispatch = useDispatch();
   let history = useHistory();
   let loginState = useSelector((state) => state.loginReducer);
-  // 42API에서 User Data 받아오기.
-  const getData = async () => {
-    try {
-      const { data: Data } = await axios.get(
-        `http://localhost:5000/auth/signin?code=${code}`
-      );
-      const {
-        token: { accessToken, refreshToken },
-      } = Data;
-      // loginReducer state 변경
-      dispatch({ type: "LOGIN", payload: Data.user });
-      const timerId = setInterval(getToken, 1000 * 60 * 29);
-      localStorage.setItem("accessToken", accessToken);
-      localStorage.setItem("refreshToken", refreshToken);
-      localStorage.setItem("timerId", timerId);
-      // 15분마다 요청
-      history.push("/");
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  getData();
+  // Token 받아오기
   const getToken = async () => {
     try {
       const prevRefreshToken = localStorage.getItem("refreshToken");
@@ -46,5 +25,29 @@ export default function AuthMain() {
       console.log(err);
     }
   };
+  // 42API에서 User Data 받아오기.
+  const getData = async () => {
+    try {
+      const { data: Data } = await axios.get(
+        `http://localhost:5000/auth/signin?code=${code}`
+      );
+      const {
+        token: { accessToken, refreshToken },
+      } = Data;
+      // loginReducer state 변경
+      dispatch({ type: "LOGIN", payload: Data.user });
+      // 29분마다 요청
+      const timerId = setInterval(getToken, 1000 * 60 * 29);
+      localStorage.setItem("accessToken", accessToken);
+      localStorage.setItem("refreshToken", refreshToken);
+      localStorage.setItem("timerId", timerId);
+      history.push("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   return loginState === null && <ReactLoading type="spin" color="#a7bc5b" />;
 }

0a7f446073ba0444e651cf8a5e5158ca27a6835a
djeon <du0928@naver.com>
2021-10-29 00:27:56 +0900

    arrange

--- back/src/routes/project/project.service.ts
+++ back/src/routes/project/project.service.ts
@@ -1148,6 +1148,24 @@ export const unlikeProject = async (request: Request, response: Response) => {
     });
 }
 
+export const modifyPosition = async (request: Request, response: Response) => {
+    const { projectId, position } = request.params;
+
+    if (projectId === undefined || position === undefined) {
+        response.status(400).json({ errMessage: 'please input projectId or position value' });
+        return ;
+    }
+    await Projectprofile.update({
+        position: position
+    }, { where: { projectId: projectId, profileId: request.user!.id } })
+    .then(() => {
+        response.status(200).json({ message: 'updated successfully.' });
+    })
+    .catch(err => {
+        response.status(405).json({ errMessage: String(err) });
+    });
+}
+
 export const deletePosition = async (request: Request, response: Response) => {
     const { projectId, position } = request.params;
 
@@ -1233,22 +1251,4 @@ export const checkInterestProject = async (request: Request, response: Response)
     } else {
         response.status(200).json({ message: 'true' });
     }
-}
-
-export const modifyPosition = async (request: Request, response: Response) => {
-    const { projectId, position } = request.params;
-
-    if (projectId === undefined || position === undefined) {
-        response.status(400).json({ errMessage: 'please input projectId or position value' });
-        return ;
-    }
-    await Projectprofile.update({
-        position: position
-    }, { where: { projectId: projectId, profileId: request.user!.id } })
-    .then(() => {
-        response.status(200).json({ message: 'updated successfully.' });
-    })
-    .catch(err => {
-        response.status(405).json({ errMessage: String(err) });
-    });
 }

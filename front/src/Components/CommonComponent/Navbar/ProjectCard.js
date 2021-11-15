e754ff783f791681e2c6d04a53331338ae29bab3
jiyong LEE <jyman1995@gmail.com>
2021-11-15 19:16:58 +0900

    feat: #445 Add Image Resize

--- front/src/Components/CommonComponent/Navbar/ProjectCard.js
+++ front/src/Components/CommonComponent/Navbar/ProjectCard.js
@@ -1,8 +1,29 @@
-import react from "react";
+import react, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default function ProjectCard({ title, image, id, setSearchRes }) {
+  const [imageUrl, setImageUrl] = useState();
+  useEffect(() => {
+    const resizedImage = async (key, size) => {
+      // 정상적으로 가져와지면 resized url반환, 아니면 원본이미지 url반환
+      const url = key.split("/");
+      url[url.length - 3] = size;
+      const resized = url.join("/");
+      try {
+        await axios({
+          method: "head",
+          url: `${resized}?timestamp=${Date.now()}`,
+        });
+        setImageUrl(resized);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    resizedImage(image, "500");
+  }, []);
   return (
-    <a className="link-color" href={`/project/${id}`}>
+    <Link className="link-color" to={`/project/${id}`}>
       <div
         className="res__projectCard"
         onClick={() => {
@@ -12,10 +33,10 @@ export default function ProjectCard({ title, image, id, setSearchRes }) {
         }}
       >
         <div className="image">
-          <img src={image} alt={`image${id}`}></img>
+          <img src={imageUrl} alt={`image${id}`}></img>
         </div>
         <div className="title">{title}</div>
       </div>
-    </a>
+    </Link>
   );
 }
--- front/src/Components/CommonComponent/Navbar/SearchCard.js
+++ front/src/Components/CommonComponent/Navbar/SearchCard.js
@@ -1,21 +1,43 @@
-import react from "react";
+import react, { useEffect, useState } from "react";
 import default_intra from "../../../default_intra.png";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default function UserCard({ name, profile, id, setSearchRes }) {
+  const [profileUrl, setProfileUrl] = useState();
+  useEffect(() => {
+    const resizedImage = async (key, size) => {
+      // 정상적으로 가져와지면 resized url반환, 아니면 원본이미지 url반환
+      const url = key.split("/");
+      url[url.length - 3] = size;
+      const resized = url.join("/");
+      try {
+        await axios({
+          method: "head",
+          url: `${resized}?timestamp=${Date.now()}`,
+        });
+        setProfileUrl(resized);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    resizedImage(profile, "100");
+  }, []);
   return (
-    <a className="link-color" href={`/profile/${id}`}>
+    <Link className="link-color" to={`/profile/${id}`}>
       <div
         className="res__userCard"
         onClick={() => {
           const inputEl = document.querySelector(".Nav__input input");
           inputEl.value = "";
           setSearchRes([]);
-        }}>
+        }}
+      >
         <div className="profile">
-          <img src={profile || default_intra} alt={`profile${id}`}></img>
+          <img src={profileUrl || default_intra} alt={`profile${id}`}></img>
         </div>
         <div className="name">{name}</div>
       </div>
-    </a>
+    </Link>
   );
 }

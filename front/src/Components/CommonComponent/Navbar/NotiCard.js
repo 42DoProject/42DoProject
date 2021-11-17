b2fa49b28200dac79b7d3e4007634afb108f03b6
jiyong LEE <jyman1995@gmail.com>
2021-11-17 21:00:48 +0900

    fix: #482 fix Err

--- front/src/Components/CommonComponent/Navbar/NotiCard.js
+++ front/src/Components/CommonComponent/Navbar/NotiCard.js
@@ -31,8 +31,8 @@ export default function NotiCard({ date, type, args }) {
             <div className="title">팔로우</div>
             <div className="date">{relativeTime(date)}</div>
           </div>
-          <span className="userName">{userName}</span>님이 팔로우
-          신청하였습니다.
+          <span className="userName">{userName}</span>님이 팔로우하기
+          시작했어요.
         </div>
       )}
       {type === 20 && (
@@ -46,8 +46,8 @@ export default function NotiCard({ date, type, args }) {
             <div className="title">팔로워</div>
             <div className="date">{relativeTime(date)}</div>
           </div>
-          <span className="userName">{userName}</span>님이 프로젝트 찾는 중으로
-          상태를 변경하였습니다.
+          <span className="userName">{userName}</span>님이 프로젝트를 찾기
+          시작했어요.{" "}
         </div>
       )}
       {type === 40 && (
@@ -58,11 +58,11 @@ export default function NotiCard({ date, type, args }) {
           }}
         >
           <div className="row1">
-            <div className="title">Project 신청</div>
+            <div className="title">프로젝트 신청</div>
             <div className="date">{relativeTime(date)}</div>
           </div>
-          <span className="projectName">{args[0].projectName}</span>에
-          참여신청되었습니다.
+          <span className="projectName">{args[0].projectName}</span>에 참여를
+          신청했어요.
         </div>
       )}
       {type === 41 && (
@@ -73,11 +73,11 @@ export default function NotiCard({ date, type, args }) {
           }}
         >
           <div className="row1">
-            <div className="title">Project 승인</div>
+            <div className="title">프로젝트 승인</div>
             <div className="date">{relativeTime(date)}</div>
           </div>
           <span className="projectName">{args[0].projectName}</span>에
-          참여되었습니다.
+          참여되었어요.
         </div>
       )}
       {type === 42 && (
@@ -88,7 +88,7 @@ export default function NotiCard({ date, type, args }) {
           }}
         >
           <div className="row1">
-            <div className="title">Project 거절</div>
+            <div className="title">프로젝트 참여 거절</div>
             <div className="date">{relativeTime(date)}</div>
           </div>
           <span className="projectName">{args[0].projectName}</span>에 참여하지
@@ -103,13 +103,13 @@ export default function NotiCard({ date, type, args }) {
           }}
         >
           <div className="row1">
-            <div className="title">Project 지원</div>
+            <div className="title">프로젝트 참여 요청</div>
             <div className="date">{relativeTime(date)}</div>
           </div>
           <span className="projectName">{args[1].projectName}</span>
           {`에 `}
-          <span className="projectName">{args[0].username}</span>가
-          참여신청했습니다.
+          <span className="projectName">{args[0].username}</span>님이 참여를
+          신청했어요.
         </div>
       )}
       {type === 60 && (
@@ -120,26 +120,26 @@ export default function NotiCard({ date, type, args }) {
           }}
         >
           <div className="row1">
-            <div className="title">Project</div>
+            <div className="title">프로젝트 상태</div>
             <div className="date">{relativeTime(date)}</div>
           </div>
           <span className="projectName">{args[0].projectName}</span>가
           {args[1].status === 0 && (
             <>
-              <span className="projectName"> 모집 중</span>
+              <span className="projectName"> 모집중</span>
               <span>으</span>
             </>
           )}
           {args[1].status === 1 && (
             <>
-              <span className="projectName"> 진행 중</span>
+              <span className="projectName"> 진행중</span>
               <span>으</span>
             </>
           )}
           {args[1].status === 2 && (
             <span className="projectName"> 완료 상태</span>
           )}
-          로 변경되었습니다.
+          로 변경되었어요.
         </div>
       )}
     </div>
